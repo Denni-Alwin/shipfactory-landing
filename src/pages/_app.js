9298e8be2246e4fb1f32251b7836ea1b52f35d7b
NOMAD <65033982+Denni-Alwin@users.noreply.github.com>
2025-09-03 16:42:51 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,21 +22,21 @@ export default function App({ Component, pageProps }) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="./apple-touch-icon.png"
+          href="./favicon/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="./favicon-32x32.png"
+          href="./favicon/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="./favicon-16x16.png"
+          href="./favicon/favicon-16x16.png"
         />
-        <link rel="manifest" href="./site.webmanifest" />
+        <link rel="manifest" href="./favicon/site.webmanifest" />
       </Head>
       <main
         className={`${display.variable} ${body.variable} flex min-h-screen flex-col font-body text-base-600 dark:text-base-500 bg-base-50 dark:bg-base-950`}
